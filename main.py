de81486e87481604ee57d6c83d3800ae4beff2d4
ArturJustel <arturjusthtel@gmail.com>
2021-11-30 12:02:56 +0100

    add image processing part

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import os
+import numpy as np
 
+# The following function reads the folder with images.
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
@@ -9,8 +11,16 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
+def find_characters(a_training_dataset):
+
+    for image in a_training_dataset:
+        imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # convert the image to grayscale format
+        ret, thresh = cv.threshold(imgray, 127, 255, 0)
+        im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    return None
+
 def main():
     training_dataset = load_images_from_folder("data/training")
-    
+
 if __name__ == "__main__":
     main()

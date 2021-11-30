475e30c9590ea33a25dd7c92af3e4ebb1d364154
ArturJustel <arturjusthtel@gmail.com>
2021-11-30 11:51:51 +0100

    re-name folder and read the data

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import cv2
+import os
 
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+def main():
+    training_dataset = load_images_from_folder("data/training")
+    
+if __name__ == "__main__":
+    main()

431016fe5988add589eb28687b9137adbcdf7c1f
ArturJustel <arturjusthtel@gmail.com>
2021-11-30 13:19:11 +0100

    Add image processing

--- main.py
+++ main.py
@@ -12,15 +12,29 @@ def load_images_from_folder(folder):
     return images
 
 def find_characters(a_training_dataset):
-
     for image in a_training_dataset:
-        imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # convert the image to grayscale format
-        ret, thresh = cv.threshold(imgray, 127, 255, 0)
-        im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        # convert the image to grayscale format 
+        imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        thresh = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,23)
+        # make monorom
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+        # remove noise with the help of contourArea
+        cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        for c in cnts:
+            area = cv2.contourArea(c)
+            if area < 10:
+                cv2.drawContours(opening, [c], -1, (0,0,0), -1)
+                
+        cv2.imshow('thresh', opening)
+        cv2.waitKey()
+
+
     return None
 
 def main():
     training_dataset = load_images_from_folder("data/training")
-
+    find_characters(training_dataset)
 if __name__ == "__main__":
     main()

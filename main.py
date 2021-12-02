49a61fcd584647e506c424cb61d785b6225714ab
ArturJustel <arturjusthtel@gmail.com>
2021-12-02 13:47:15 +0100

    Add lable getter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import os
-import numpy as np
+import pytesseract
+ 
 
 # The following function reads the folder with images.
 def load_images_from_folder(folder):
@@ -11,30 +12,38 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
-def find_characters(a_training_dataset):
-    for image in a_training_dataset:
-        # convert the image to grayscale format 
-        imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        thresh = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,23)
-        # make monorom
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-        # remove noise with the help of contourArea
-        cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        for c in cnts:
-            area = cv2.contourArea(c)
-            if area < 10:
-                cv2.drawContours(opening, [c], -1, (0,0,0), -1)
-                
-        cv2.imshow('thresh', opening)
-        cv2.waitKey()
+# The following function obtains the captha lable for the file name.
+def get_captha_lable(folder):
+    lables = []
+    for filename in os.listdir(folder):
+        file = os.path.basename(filename)
+        lable = file.split(".")[0]
+        lables.append(lable)
+    return lables
 
 
-    return None
+# The following function transforms the captha image into black and white image.
+def preprocess(image):
+    # convert the image to grayscale format 
+    imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(imgray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,23)
+    # make monorom
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+    # remove noise with the help of contourArea
+    cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area < 10:
+            cv2.drawContours(opening, [c], -1, (0,0,0), -1)
+    result = 255 - opening
+
+    return result,opening
 
 def main():
     training_dataset = load_images_from_folder("data/training")
-    find_characters(training_dataset)
+    training_lables = get_captha_lable("data/training")
+    preprocess(training_dataset)
 if __name__ == "__main__":
     main()

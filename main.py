b3719082aa569c3c6b3204fb9c828ccc81a99e47
ArturJustel <arturjusthtel@gmail.com>
2021-12-07 12:26:21 +0100

    Add prediction model

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from preprocessing import preprocess
 def main():
     training_dataset = preprocess.load_images_from_folder("data/training")
     training_lables = preprocess.get_captha_lable("data/training")
-    preprocess.preprocess(training_dataset[0])
+    preprocess.Classifier(training_dataset,training_lables)
 if __name__ == "__main__":
     main()
--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,10 @@
+from PIL.Image import NONE
 import cv2
 import os
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
 
+# This is a class that contains all the pre-processing functions.
 class preprocess:
     # The following function reads the folder with images.
     def load_images_from_folder(folder):
@@ -21,6 +25,21 @@ class preprocess:
         return lables
 
 
+    # This function performs KNearestNeighborClassifier on preprocessed image.
+    def Classifier(images,lables):
+        preProcessedImages = []
+        for image in images:
+            preProcessedImages.append(preprocess.preprocess(image))
+        d3array = np.array(preProcessedImages)
+        nsamples, nx, ny = d3array.shape
+        d2_train_dataset = d3array.reshape((nsamples,nx*ny))
+        knn = KNeighborsClassifier(n_neighbors=5, p=2, weights='uniform', algorithm='auto')
+        knn.fit(d2_train_dataset,lables)
+        print(knn.predict(d2_train_dataset))
+        print(lables)
+        print('Training accuracy score: %.3f' % knn.score(d2_train_dataset, lables))
+        
+
     # The following function transforms the captha image into black and white image.
     def preprocess(image):
         # convert the image to grayscale format 
@@ -36,7 +55,5 @@ class preprocess:
             area = cv2.contourArea(c)
             if area < 10:
                 cv2.drawContours(opening, [c], -1, (0,0,0), -1)
-        result = 255 - opening
-        cv2.imshow('thresh',result)
-        cv2.waitKey(0)
-        # return result,opening
+        return opening
+

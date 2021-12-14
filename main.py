5a24d44c24be373b0ce2f916b0b7907e2aba15d7
postmapetersimon <postmapetersimon@gmail.com>
2021-12-14 12:03:52 +0100

    Only select first image

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def main():
     # preprocess(random.choice(training_dataset))
     erosion_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
     dilation_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-    preprocess(image=random.choice(training_dataset), image_height=100, blur=(5,5), dilation_kernel=dilation_kernel, erosion_kernel=erosion_kernel)
+    # preprocess(image=random.choice(training_dataset), image_height=100, blur=(5,5), dilation_kernel=dilation_kernel, erosion_kernel=erosion_kernel)
+    preprocess(training_dataset[0], image_height=100, blur=(5,5), dilation_kernel=dilation_kernel, erosion_kernel=erosion_kernel)
 
 
 
